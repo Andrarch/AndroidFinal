e4bb865e9f38947c0a73db1e18ac7ec6af2d4243
alkh0084 <alkh0084@algonquinlive.com>
2018-04-04 13:22:31 -0400

    Wed 4

--- app/src/main/java/com/example/andrew/androidfinal/ShadiMovies.java
+++ app/src/main/java/com/example/andrew/androidfinal/ShadiMovies.java
@@ -1,8 +1,10 @@
 package com.example.andrew.androidfinal;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,23 +17,54 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class ShadiMovies extends Activity {
     ProgressBar progressBar;
     ListView listView;
     EditText editText;
     Button button;
+    ArrayList<String> arrayListMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shadi_movies);
+        final ContentValues cv=new ContentValues();
+        arrayListMovies = new ArrayList<>();
         progressBar = findViewById(R.id.progressBarId);
+        button=findViewById(R.id.buttonAddId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editText.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Message is empty", Toast.LENGTH_LONG).show();
+                } else {
+                    String str = editText.getText().toString();
+                    arrayListMovies.add(str);
+                    //cv.put(cdh.KEY_MESSAGE, str);
+                    //db.insert(cdh.TABLE_NAME, "null",cv);
+                    //listViewChat.setAdapter(messageAdapter);
+                   // messageAdapter.notifyDataSetChanged(); //this restarts the process of getCount() & getView()
+                    editText.setText("");
+                }
+
+
+            }
+
+
+
+        });
         editText=findViewById(R.id.editTextMovieId);
         listView=findViewById(R.id.listViewId);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
-                
+                Snackbar snackbar = Snackbar
+                        .make(button, "www.journaldev.com", Snackbar.LENGTH_LONG);
+                snackbar.show();
+
+
                 Intent intent = new Intent(ShadiMovies.this, ListItemActivity.class);
                 startActivity(intent);
             }
