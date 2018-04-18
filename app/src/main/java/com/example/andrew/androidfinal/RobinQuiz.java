5e576c088a749776a018a63c54533a76d56eb25d
robinshrstha <robinshrestha.oslt@gmail.com>
2018-04-18 11:32:17 -0400

    Merge branch 'master' of https://github.com/Andrarch/AndroidFinal into RobinQuizBranch
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/andrew/androidfinal/RobinQuiz.java

--- app/src/main/java/com/example/andrew/androidfinal/RobinQuiz.java
+++ app/src/main/java/com/example/andrew/androidfinal/RobinQuiz.java
@@ -25,7 +25,7 @@ public class RobinQuiz extends AppCompatActivity {
 
     SearchDatabaseHelper searchDatabase;
     EditText javaEditText;
-    Button infoButton;
+    Button multiple, trueFalse, numeric, infoButton;
     SQLiteDatabase databaseDb;
     Cursor cursor;
 }
@@ -35,7 +35,10 @@ public class RobinQuiz extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_robin_quiz);
         infoButton = findViewById(R.id.sendButton);
-        TextView message = (TextView) result.findViewById(R.id.textView);
+        multiple = findViewById(R.id.sendButton);
+        trueFalse = findViewById(R.id.sendButton);
+        numeric = findViewById(R.id.sendButton);
+       // TextView message = (TextView) result.findViewById(R.id.textView);
 
     Snackbar snackbar = Snackbar.make(findViewById(R.id.textView), "Quiz data", Snackbar.LENGTH_LONG);
 
@@ -47,25 +50,25 @@ public class RobinQuiz extends AppCompatActivity {
 //
 //        });
 
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getContext());
-        alertBuilder.setMessage("Stop Number: "+message.getText())
-                .setPositiveButton(R.string.Continue, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Intent intent = new Intent(RobinQuiz.this, RobinQuiz.class);
-                        intent.putExtra("StopNumber", message.getText());
-                        startActivity(intent);
-                    }
-                })
-                .setNegativeButton(R.string.Delete, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Toast toast = Toast.makeText(RobinQuiz.this, "Placeholder for Delete", Toast.LENGTH_LONG);
-                        toast.show();
-                    }
-                });
-
-
-        AlertDialog alert = alertBuilder.create();
-        alert.show();
+//        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getContext());
+//        alertBuilder.setMessage("Stop Number: "+message.getText())
+//                .setPositiveButton(R.string.Continue, new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        Intent intent = new Intent(RobinQuiz.this, RobinQuiz.class);
+//                        intent.putExtra("StopNumber", message.getText());
+//                        startActivity(intent);
+//                    }
+//                })
+//                .setNegativeButton(R.string.Delete, new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        Toast toast = Toast.makeText(RobinQuiz.this, "Placeholder for Delete", Toast.LENGTH_LONG);
+//                        toast.show();
+//                    }
+//                });
+//
+//
+//        AlertDialog alert = alertBuilder.create();
+//        alert.show();
 
 //    });
 //        button.setOnClickListener(new View.OnClickListener() {
