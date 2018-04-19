1cd775492e29176e1af7ae65978bbbf779684f08
robinshrstha <robinshrestha.oslt@gmail.com>
2018-04-19 14:05:41 -0400

    update6th

---
+++ app/src/main/java/com/example/andrew/androidfinal/RobinDatabaseHelper.java
@@ -0,0 +1,49 @@
+package com.example.andrew.androidfinal;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Robin on 3/7/2018.
+ */
+
+public class RobinDatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Messages.db";
+
+    public static final int VERSION_NUM = 4;
+
+    public static final String TABLE_NAME = "myTable";
+
+    public static final String KEY_ID = "id";
+
+    public static final String KEY_QUIZ = "quiz";
+
+
+
+    public RobinDatabaseHelper(Context ctx) {
+        super(ctx, DATABASE_NAME, null, VERSION_NUM);
+
+    }
+
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL( "CREATE TABLE " + TABLE_NAME + "( "+KEY_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+KEY_QUIZ+" text);");
+
+        Log.i("ChatDatabaseHelper", "Calling onCreate");
+    }
+
+    public void onUpgrade (SQLiteDatabase db, int oldVer, int newVer){
+
+        db.execSQL("DROP TABLE IF EXISTS " +TABLE_NAME+";");
+
+        onCreate(db);
+
+        Log.i("ChatDatabaseHelper", "Calling onUpgrade, oldVersion=" + oldVer + " newVersion=" + newVer);
+    }
+    public void onOpen(SQLiteDatabase db)
+    {
+        Log.i("Database", "Database opened");
+    }
+}
+
