1c5b3a618a6a4565cf5e9056fd988ecd82f5d2b9
Andrew <andrew.archibald1@gmail.com>
2018-04-20 14:04:31 -0400

    Finished - taskbar done

--- app/src/main/java/com/example/andrew/androidfinal/MainActivity.java
+++ app/src/main/java/com/example/andrew/androidfinal/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.andrew.androidfinal;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+
+
+
+
         Button btnAndrewMain=findViewById(R.id.MainAndrewButton);
         btnAndrewMain.setOnClickListener((click)->{
             Intent intent = new Intent(MainActivity.this, AndrewTranspo.class);
@@ -37,5 +52,48 @@ public class MainActivity extends Activity {
             startActivity(intent);
         });
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu m){
+        getMenuInflater().inflate(R.menu.maintoolbar, m );
+        return true;
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem mi){
+        int id=mi.getItemId();
+        Intent intent;
+        switch (id){
+            case R.id.action_andrew:
+                Log.d("ToolBar", "option 1");
+                intent= new Intent(MainActivity.this, AndrewTranspo.class);
+
+                startActivity(intent);
+                break;
+
+
+            case R.id.action_shadi:
+                Log.d("ToolBar", "option 2");
+                intent = new Intent(MainActivity.this, ShadiMovies.class);
+
+                startActivity(intent);
+
+                break;
+            case R.id.action_bilal:
+                Log.d("ToolBar", "option 3");
+                intent = new Intent(MainActivity.this, BilalClinic.class);
+
+                startActivity(intent);
+
+                break;
+            case R.id.action_robin:
+                Log.d("ToolBar", "option 4");
+                intent = new Intent(MainActivity.this, RobinQuiz.class);
+
+                startActivity(intent);
+                break;
+        }
+
+        return true;
     }
 }
