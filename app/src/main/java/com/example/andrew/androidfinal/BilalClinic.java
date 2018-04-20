9542b58cb87492d4dc40a8c1eabdb0a9022bd957
Bilal <billownah@gmail.com>
2018-04-20 15:39:29 -0400

    bilal-final commit

--- app/src/main/java/com/example/andrew/androidfinal/BilalClinic.java
+++ app/src/main/java/com/example/andrew/androidfinal/BilalClinic.java
@@ -1,36 +1,41 @@
 package com.example.andrew.androidfinal;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+/**
+ * BilalClinic class to show current patients or add new ones
+ */
 public class BilalClinic extends Activity {
 
     Button b;
-    Button resetB;
+//    Button resetB;
 
+    /**
+     * adds functionality to buttons to access other activities
+     * @param savedInstanceState loads after reloading
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bilal_clinic);
 
-        b=findViewById(R.id.fillFormButton);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(BilalClinic.this, "Transferring to form", Toast.LENGTH_SHORT).show();
-            }
+        b = findViewById(R.id.newPatientButton);
+        b.setOnClickListener((t) -> {
+
+            Toast.makeText(BilalClinic.this, "Transferring to form", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(BilalClinic.this, NewPatientForm.class);
+
+            startActivity(intent);
+
         });
 
-        resetB=findViewById(R.id.resetButton);
-        resetB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Toast.makeText(BilalClinic.this, "Transferring to form", Toast.LENGTH_SHORT).show();
-            }
-        });
+
+
+
     }
 }
